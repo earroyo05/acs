ebeb901cb749371f1578712ad3d90056644a606b
earroyo05 <146686879+earroyo05@users.noreply.github.com>
2025-11-07 17:03:34 -0600

    Update tailwind.config.js - Colores del Brandbook

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'verde-camposanto': '#4A765A',
+        'gris-piedra': '#717A76',
+        'verde-lino': '#C7D7C1',
+        'blanco-lino': '#F7F7F7',
+      },
+      fontFamily: {
+        serif: ['"Trajan Pro"', 'serif'],
+        sans: ['Lato', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
