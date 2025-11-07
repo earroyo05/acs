af67de7433fa18b4f58db822a0b0ef7c12503727
earroyo05 <146686879+earroyo05@users.noreply.github.com>
2025-11-07 17:04:30 -0600

    Update _app.js - Utilizacion de Fonts Corporativos

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,22 @@
-import '../styles/globals.css'
+import '@/styles/globals.css';
+import { Lato } from 'next/font/google';
+import localFont from 'next/font/local';
+
+const lato = Lato({
+  subsets: ['latin'],
+  weight: ['300', '400', '700'],
+  variable: '--font-lato',
+});
+
+const trajan = localFont({
+  src: '../public/fonts/TrajanPro-Regular.ttf',
+  variable: '--font-trajan',
+});
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${lato.variable} ${trajan.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
